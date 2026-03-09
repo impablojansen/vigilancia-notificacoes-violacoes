d00ed1bf6afd8f9abe378cc22601bb327dd3e8a2
impablojansen <impablojansen@gmail.com>
2026-03-09 11:22:16 -0300

    corrige refs nome e nasc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const nome = ref('')
+const nasc = ref<Date | null>(null)
+</script>
 
 <template>
     <div class="card flex justify-center">
@@ -8,25 +13,32 @@
                 <Step value="2">Violação</Step>
                 <Step value="3">Encaminhamento</Step>
             </StepList>
+
             <StepPanels>
                 <StepPanel v-slot="{ activateCallback }" value="1">
                     <div class="flex flex-col gap-4">
                         <h3 class="text-lg">Dados do Notificado</h3>
+
                         <FloatLabel variant="on">
-                        <InputText id="nome" v-model="nome" fluid />
-                        <label for="nome">Nome Completo</label>
+                            <InputText id="nome" v-model="nome" fluid />
+                            <label for="nome">Nome Completo</label>
                         </FloatLabel>
+
                         <div class="flex gap-2">
-                        <DatePicker v-model="nasc" placeholder="Data de Nascimento" fluid />
+                            <DatePicker v-model="nasc" placeholder="Data de Nascimento" fluid />
                         </div>
                     </div>
+
                     <div class="flex pt-6 justify-end">
-                        <Button label="Próximo" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback('2')" />
+                        <Button
+                            label="Próximo"
+                            icon="pi pi-arrow-right"
+                            iconPos="right"
+                            @click="activateCallback('2')"
+                        />
                     </div>
                 </StepPanel>
             </StepPanels>
         </Stepper>
     </div>
 </template>
-
-<style scoped></style>
